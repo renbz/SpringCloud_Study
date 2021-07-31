c24088e0e37e7ba69319a92135d232ba9f6d837f
renbz <1362206371@qq.com>
2021-07-31 11:21:01 +0800

    record

--- R06_Springcloud-Config-Server-3333/src/main/java/com/ren/springcloud/Config_Server_3333.java
+++ R06_Springcloud-Config-Server-3333/src/main/java/com/ren/springcloud/Config_Server_3333.java
@@ -10,10 +10,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class Config_Server_3344 {
+public class Config_Server_3333 {
 
     public static void main(String[] args) {
-        SpringApplication.run(Config_Server_3344.class,args);
+        SpringApplication.run(Config_Server_3333.class,args);
     }
-
 }
